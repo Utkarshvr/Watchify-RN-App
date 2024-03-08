4becf77625279d72a448fecdead959dea266e39a
Utkarsh Verma <108287933+Utkarshvr@users.noreply.github.com>
2024-03-09 01:34:14 +0530

    made  channel settings functional

--- src/Screens/ChannelSettingsScreen.jsx
+++ src/Screens/ChannelSettingsScreen.jsx
@@ -13,25 +13,29 @@ import {
   TextareaInput,
 } from "@gluestack-ui/themed";
 import useBGColor from "../hooks/useBGColor";
-import { useAuthData } from "../context/AuthContext";
+import { useAuthAPI, useAuthData } from "../context/AuthContext";
 import { Image, TouchableOpacity } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import { Text } from "@gluestack-ui/themed";
 import { Input } from "@gluestack-ui/themed";
 import { useEffect, useState } from "react";
+import axiosInstance from "../utils/axiosInstance";
+import { useNotificationAPI } from "../context/NotificationContext";
+import { getUserRoute } from "../utils/api/api.routes";
 
 export default function ChannelSettingsScreen() {
   const { bgColor } = useBGColor();
   const { user } = useAuthData();
+  const { setAuthToken, setUser, setIsLoading } = useAuthAPI();
+  const { openNotification } = useNotificationAPI();
 
   // User Info:
-
+  const [isModified, setIsModified] = useState(false);
   const [userPicture, setUserPicture] = useState({ uri: "" });
   const [userBanner, setUserBanner] = useState({ uri: "" });
   const [userName, setUserName] = useState("");
   const [userHandle, setUserHandle] = useState("");
   const [userDesc, setUserDesc] = useState("");
-  console.log({ userPicture, userBanner });
   const [links, setLinks] = useState([]);
 
   const channelURLLink = `${process.env.EXPO_PUBLIC_WEBSITE_BASE_URL}/channel/${user?.channelID}`;
@@ -42,9 +46,10 @@ export default function ChannelSettingsScreen() {
       setUserName(user?.name);
       setUserHandle(user?.user_handle);
       setUserDesc(user?.desc);
-      setUserPicture({ uri: user?.picture });
-      setUserBanner({ uri: user?.banner_image || "" });
+      // setUserPicture({ uri: user?.picture });
+      // setUserBanner({ uri: user?.banner_image || "" });
       setLinks(user?.links);
+      setIsModified(false);
     }
   }, [user]);
 
@@ -57,6 +62,7 @@ export default function ChannelSettingsScreen() {
     });
 
     if (!result.canceled) {
+      setIsModified(true);
       const img = result.assets[0];
       setUserBanner(img);
     }
@@ -70,17 +76,69 @@ export default function ChannelSettingsScreen() {
     });
 
     if (!result.canceled) {
+      setIsModified(true);
       const img = result.assets[0];
       setUserPicture(img);
     }
   };
+
+  async function customizeChannel() {
+    try {
+      const formData = new FormData();
+      formData.append("name", userName);
+      formData.append("user_handle", userHandle);
+      formData.append("desc", userDesc);
+
+      if (userPicture?.uri)
+        formData.append("user_picture", {
+          uri: userPicture?.uri,
+          name: userPicture?.uri?.split("/")[userPicture?.uri?.split("/")?.length - 1],
+          type: `image/${userPicture?.uri?.split(".").pop()}`,
+        });
+      if (userBanner?.uri)
+        formData.append("banner_image", {
+          uri: userBanner?.uri,
+          name: userBanner?.uri?.split("/")[userBanner?.uri?.split("/")?.length - 1],
+          type: `image/${userBanner?.uri?.split(".").pop()}`,
+        });
+
+      await axiosInstance.post("/user/me/customize", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+      openNotification({ title: "Channel Changes are completed" });
+
+      setIsLoading(true);
+      const { data: userData } = await axiosInstance.get(getUserRoute);
+      console.log({ userData });
+
+      if (userData) {
+        setIsLoading(false);
+        setUser(userData?.data?.user);
+      } else {
+        setIsLoading(false);
+        setUser(null);
+        setAuthToken(null);
+      }
+    } catch (error) {
+      console.log(error);
+      openNotification({ title: error?.message }, "error");
+    }
+  }
 
   return (
     <Box bgColor={bgColor} flex={1}>
       <ScrollView mb={"$10"} bgColor={bgColor} flex={1}>
         <Box position="relative">
           <Image
-            source={userBanner?.uri ? { uri: userBanner?.uri } : require("../assets/no-banner.jpg")}
+            source={
+              userBanner?.uri
+                ? { uri: userBanner?.uri }
+                : user?.banner_image
+                  ? { uri: user?.banner_image }
+                  : require("../assets/no-banner.jpg")
+            }
             style={{ width: "100%", height: 100 }}
           />
           <Box
@@ -93,7 +151,10 @@ export default function ChannelSettingsScreen() {
             justifyContent="center"
           >
             <Box position="relative" alignItems="center" justifyContent="center">
-              <Image source={{ uri: userPicture?.uri }} style={{ width: 72, height: 72, borderRadius: 999 }} />
+              <Image
+                source={{ uri: userPicture?.uri || user?.picture }}
+                style={{ width: 72, height: 72, borderRadius: 999 }}
+              />
 
               {/* For User Picture */}
               <Box
@@ -119,7 +180,14 @@ export default function ChannelSettingsScreen() {
           <Box p="$2">
             <Text>Name</Text>
             <Input isDisabled={false} variant="underlined" size="sm">
-              <InputField placeholder="Luke..." onChangeText={(text) => setUserName(text)} value={userName} />
+              <InputField
+                placeholder="Luke..."
+                onChangeText={(text) => {
+                  setIsModified(true);
+                  setUserName(text);
+                }}
+                value={userName}
+              />
             </Input>
           </Box>
           <Box p="$2">
@@ -127,7 +195,10 @@ export default function ChannelSettingsScreen() {
             <Input isDisabled={false} variant="underlined" size="sm">
               <InputField
                 placeholder="luke_belmar..."
-                onChangeText={(text) => setUserHandle(text)}
+                onChangeText={(text) => {
+                  setIsModified(true);
+                  setUserHandle(text);
+                }}
                 value={userHandle}
               />
             </Input>
@@ -151,15 +222,24 @@ export default function ChannelSettingsScreen() {
             <Textarea height={"$72"} size="sm" isDisabled={false} width="$full">
               <TextareaInput
                 value={userDesc}
-                onChangeText={(text) => setUserDesc(text)}
+                onChangeText={(text) => {
+                  setIsModified(true);
+                  setUserDesc(text);
+                }}
                 placeholder="Write a Description for your channel"
               />
             </Textarea>
           </Box>
         </Box>
       </ScrollView>
-      <Box position="absolute" bottom={"$4"} width="$full" justifyContent="center" alignItems="center">
-        <Button>
+      <Box
+        position="absolute"
+        bottom={!isModified ? -100 : "$4"}
+        width="$full"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Button onPress={customizeChannel} isDisabled={!isModified}>
           <ButtonText>Save Changes</ButtonText>
         </Button>
       </Box>
